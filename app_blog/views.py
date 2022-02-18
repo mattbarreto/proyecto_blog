64fe689658e94d36ff9d1e382618b7cc991209cf
Felix Barreto <matiasbarreto@gmail.com>
2022-02-17 21:35:37 -0300

    Ajustes en template padre.html
    Se necesitan recuperar funcionalidades.

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from app_blog.forms import AvatarFormulario
 from app_blog.models import Avatar, Categoria, Post
 
